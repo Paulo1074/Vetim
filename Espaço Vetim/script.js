e83720d4fea6f10364d797ee8f512574527bb67d
Paulo Otávio <pereirapaulootavio98@gmail.com>
2025-05-27 01:56:45 -0300

    Primeiro commit do projeto

---
+++ Espaço Vetim/script.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const closeBtn = document.querySelector('.close');
+
+document.querySelectorAll('.galeria img').forEach(img => {
+  img.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+modal.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
